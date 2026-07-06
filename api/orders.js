1df70f62804dd2e5a266ecc4098fcae3b0a153dc
zidandsyauqi-cell <zidandsyauqi@gmail.com>
2026-07-06 14:39:04 +0700

    Remove dummy data comment from orders.js
    Removed testing comment from the orders handler.

--- api/orders.js
+++ api/orders.js
@@ -1,7 +1,6 @@
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     
-    // DATA DUMMY UNTUK TESTING
     const dummyOrders = [
         {
             id_order: 1,
